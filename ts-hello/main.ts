8b0fe0421da8dec5c5fe489dcfae316cbad46109
Saranya <jsaran.bj@gmail.com>
2025-02-20 12:08:59 +0100

    Variable let and var

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,6 +1,8 @@
-function log(message){
-    console.log(message);
+function doSomething(){
+    for (var i=0; i<5; i++){
+        console.log(i);
+    }
+    console.log('Finally:'+i);
 }
-var message="Hello world";
 
-log(message);
+doSomething();
