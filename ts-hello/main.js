fff3926bd007cfcf7b2027e9fced5dd18301f180
Saranya <jsaran.bj@gmail.com>
2025-02-26 16:23:29 +0100

    Learn interface with function

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,7 +1,12 @@
 var drawPoint = function (point) {
     console.log("x: ".concat(point.x, ",y:").concat(point.y));
+    point.draw();
+};
+var someFunction = function () {
+    console.log("draw function");
 };
 drawPoint({
     x: 1,
-    y: 2
+    y: 2,
+    draw: someFunction
 });
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,13 +1,19 @@
 interface Point{
     x: number,
-    y: number
+    y: number,
+    draw:()=>void
 }
 
 let drawPoint = (point: Point) => {
-console.log(`x: ${point.x},y:${point.y}`);
+    console.log(`x: ${point.x},y:${point.y}`);
+    point.draw();
 };
 
+let someFunction = () => {
+    console.log("draw function")
+}
 drawPoint({
     x:1,
-    y:2
+    y:2,
+    draw:someFunction
 });
